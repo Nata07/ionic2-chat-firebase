db173904d3b911abc3c8d01dd6f875356128527c
Nata Silva <Nata Silva>
2017-06-20 22:35:55 -0300

    Configuração do firebase

---
+++ Ionic2FirebaseChat/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import {AngularFireModule, FirebaseAppConfig} from 'angularfire2';
+
+const firebaseAppConfig: FirebaseAppConfig = {
+    apiKey: "AIzaSyBZjiSVID8ANk1hantGP5PJCsl_lPSaxUA",
+    authDomain: "ionic2-firebase-chat-3e633.firebaseapp.com",
+    databaseURL: "https://ionic2-firebase-chat-3e633.firebaseio.com",
+    storageBucket: "ionic2-firebase-chat-3e633.appspot.com",
+    messagingSenderId: "418527449405"
+  };
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseAppConfig),
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}

688562c10b0495bafd9de5f67d86edc241d0d248
Nata Silva <nata.mw@hotmail.com>
2017-07-12 11:02:10 -0300

    configuração da photo na chatPage

--- Ionic2FirebaseChat/src/pages/signin/signin.ts
+++ Ionic2FirebaseChat/src/pages/signin/signin.ts
@@ -72,17 +72,4 @@ export class SigninPage {
       }).present();
   }
   
-  onHomePage(): void {
-    this.navCtrl.push(HomePage)
-      .then((hasAccess: boolean) => {
-          console.log('Autorizado: ', hasAccess)
-      })
-      .catch(err => {
-          console.log('Nao autorizado: ', err)  
-      });
-  }
-  
-  onLogout(): void{
-    this.authService.logout();
-  }
 }
